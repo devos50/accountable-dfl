1e5d528107c9d2deaa5c12b47bac5cfbccc87aac
Martijn de Vos <mdmartijndevos@gmail.com>
2024-11-22 17:10:06 +0100

    Added script

--- scripts/run_parallel_with_gossip_intervals.sh
+++ scripts/run_parallel_with_gossip_intervals.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage: ./script.sh <command_to_run_with_sfs_placeholder> <sf1> <sf1> ...
-# Example: ./script.sh "python my_script.py --success-fraction {}" 5 10 20
+# Example: ./script.sh "python my_script.py --gossip-interval {}" 5 10 20
 
 set -e
 
@@ -15,4 +15,4 @@ command_to_run=$1
 shift 1  # Remove the first argument to keep only the sample sizes
 
 # Use the provided sample sizes and run the command with each sample size in parallel
-printf "%s\n" "$@" | xargs -I {} -P "$#" bash -c "SUCCESS_FRACTION={} && echo \"Running command with sample size: \$SUCCESS_FRACTION\" && ${command_to_run/\{\}/\$SUCCESS_FRACTION} > output_\$SUCCESS_FRACTION.log 2>&1"
+printf "%s\n" "$@" | xargs -I {} -P "$#" bash -c "GOSSIP_INTERVAL={} && echo \"Running command with sample size: \$GOSSIP_INTERVAL\" && ${command_to_run/\{\}/\$GOSSIP_INTERVAL} > output_\$GOSSIP_INTERVAL.log 2>&1"
